ad31969569589c830d5383c73d8338d9821f3c82
Nikiya Simpson <nikiyasimpson@gmail.com>
2019-03-19 12:54:21 -0500

    update JSON API calls for item information

--- views.py
+++ views.py
@@ -321,25 +321,17 @@ def gdisconnect():
         return response
 
 
-# JSON APIs to view Restaurant Information
-@app.route('/restaurant/<int:restaurant_id>/menu/JSON')
-def restaurantMenuJSON(restaurant_id):
-    #restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(
-        restaurant_id=restaurant_id).all()
-    return jsonify(MenuItems=[i.serialize for i in items])
+# JSON APIs to view Catalog Information
 
+@app.route('/api/item/<int:id>/JSON')
+def ItemJSON(item_id):
+    item = session.query(Item).filter_by(id=item_id).one()
+    return jsonify(Item=item.serialize)
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
-def menuItemJSON(restaurant_id, menu_id):
-    Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
-    return jsonify(Menu_Item=Menu_Item.serialize)
-
-
-@app.route('/restaurant/JSON')
-def restaurantsJSON():
-    restaurants = session.query(Restaurant).all()
-    return jsonify(restaurants=[r.serialize for r in restaurants])
+@app.route('/api/items/JSON')
+def itemsJSON():
+    items = session.query(Item).all()
+    return jsonify(items=[i.serialize for i in items])
 
 
 # Show item catalog
